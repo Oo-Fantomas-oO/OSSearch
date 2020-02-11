e6dbb34472eeda7101a166900b220dcfe0990637
Serhii <777sirj777@gmail.com>
2020-02-11 23:28:46 +0200

    1

--- OSSearch/ViewController.m
+++ OSSearch/ViewController.m
@@ -11,14 +11,50 @@
 
 @interface ViewController ()
 
+@property (strong, nonatomic) NSMutableArray *studentsArray;
+@property (strong, nonatomic) OSStudent *student;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.studentsArray = [NSMutableArray array];
+    
+    for (int i = 0; i < 1000; i++) {
+        [self.studentsArray addObject:[OSStudent randomStudent]];
+    }
+    
+    [self.tableView reloadData];
+    
 }
 
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return [self.studentsArray count];
+    
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *identifier = @"Cell";
+    
+    self.student = [self.studentsArray objectAtIndex:indexPath.row];
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%@ %@", self.student.firstName, self.student.lastName];
+    
+    return cell;
+    
+}
 
 @end
